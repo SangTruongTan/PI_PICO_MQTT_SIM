d656eca721151d8d3959db76aab4e2457055a1a9
Sang Truong Tan <sang.t.truong@outlook.com>
2023-02-12 11:34:47 +0700

    Add Test Environment

--- testmain.c
+++ testmain.c
@@ -19,13 +19,16 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "configure.h"
+#include "stdlib.h"
+#include "string.h"
 
 /* Private includes ----------------------------------------------------------*/
 
 /* Private typedef -----------------------------------------------------------*/
 
-
 /* Private define ------------------------------------------------------------*/
+#define BUFFER_SIZE 128
+#define PHONE_LENGTH 10
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -33,12 +36,50 @@
 Configuration_t mConfigure;
 /* Private function prototypes -----------------------------------------------*/
 int main(void);
-
+void get_line_input(char *Buffer);
+void get_phone_message(char *Phone, char *Message);
+void get_phone(char *Phone);
+void get_message(char *Message);
 /* Private user code ---------------------------------------------------------*/
 int main(void) {
-    printf("Hello World!!!\r\n");
-    while(1) {
-
+    char *SourcePhone = "";
+    char *ActualSource = calloc(PHONE_LENGTH, sizeof(char));
+    char *Message = calloc(BUFFER_SIZE, sizeof(char));
+    int count = 1;
+    while (1) {
+        printf("%d. ", count);
+        if (strcmp(SourcePhone, "") == 0) {
+            // Configure Source Number
+            get_phone_message(ActualSource, Message);
+        } else {
+            // Don't need to confiure Source Number
+            get_message(Message);
+        }
+        printf("\n");
+        count += 1;
     }
     return 0;
+}
+
+void get_line_input(char *Buffer) {
+    fgets(Buffer, BUFFER_SIZE, stdin);
+    strtok(Buffer, "\n");
+}
+
+void get_phone_message(char *Phone, char *Message) {
+    get_phone(Phone);
+    get_message(Message);
+}
+
+void get_phone(char *Phone) {
+    printf("Your Phone:");
+    get_line_input(Phone);
+    printf("The Phone Number is:%s, Length=%d\n", Phone,
+           strlen(Phone));
+}
+
+void get_message(char *Message) {
+    printf("Your message:");
+    get_line_input(Message);
+    printf("The message is:%s, Length=%d\n", Message, strlen(Message));
 }
